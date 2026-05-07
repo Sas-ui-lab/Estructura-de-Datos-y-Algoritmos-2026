2555842e18deae9c9bc4bc6cd66d15d8e08f961a
sas-ui-lab <sayury.lazo@unas.edu.pe>
2026-05-07 16:21:05 -0500

    Modelo Multihilo

---
+++ c++/Calculadora_new.cpp
@@ -0,0 +1,45 @@
+//diseñen un programa que simplule una calculadora, suma,resta,multiplicacion y division
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a,b;
+    int opc;
+
+
+    cout<< "---- CALCULADORA ----"<<endl;
+    cout<< "que operación va a realizar?"<<endl;
+    cout<< "1. SUMA \n 2. RESTA \n 3. MULTIPLICACIÓN \n 4. DIVISION"<<endl;
+    cin>> opc;
+
+    if (c=1){
+    cout<<"usted realizara una suma";
+    cout<<"ingrese su primer numero";
+    cin>> a;
+    cout<<"ingrese su segundo numero";
+    cin>> b;
+    cout<<a+b;
+    }
+
+        if (c=2){
+    cout<<"usted realizara una resta";
+    cout<<"ingrese su primer numero";
+    cin>> a;
+    cout<<"ingrese su segundo numero";
+    cin>> b;
+    cout<<a-b;
+    }
+
+        if (c=3){
+    cout<<"usted realizara una multiplicación";
+    cout<<"ingrese su primer numero";
+    cin>> a;
+    cout<<"ingrese su segundo numero";
+    cin>> b;
+    cout<<a*b;
+    }
+}
+//para la proxima clase mejorar el codigo, ponganle opcion
+
+
